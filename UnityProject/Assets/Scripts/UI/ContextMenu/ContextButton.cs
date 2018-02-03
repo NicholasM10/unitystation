e827e0687854d807ee152e765390496d7988d8de
Maxwell Phillips <thechilisource@gmail.com>
2018-02-03 19:36:38 +0100

    Started building menu

---
+++ UnityProject/Assets/Scripts/UI/ContextMenu/ContextButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ContextButton : MonoBehaviour {
+	public GameObject target;
+
+	private void Awake()
+	{
+		gameObject.GetComponentInChildren<Text>().text = target.name;
+		gameObject.transform.Find("Image").GetComponent<Image>().sprite = target.GetComponentInChildren<SpriteRenderer>().sprite;
+	}
+}
